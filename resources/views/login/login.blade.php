cba5769fa4bfdfab167826f8b439cc21dda533aa
Yanna Tiongson <yannationgson@gmail.com>
2015-08-13 14:09:05 +0800

    [TASK] changed background, logo and button

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -8,15 +8,8 @@
             <div class="inner-bg">
                 <div class="container">
                     <div class="row">
-                        <div class="col-sm-8 col-sm-offset-2 text">
-                            <h1><strong>Bootstrap</strong> Login Form</h1>
-                            <div class="description">
-                            	<p>
-	                            	This is a free responsive login form made with Bootstrap. 
-	                            	Download it on <a href="http://azmind.com"><strong>AZMIND</strong></a>, customize and use it as you like!
-                            	</p>
-                            </div>
-                        </div>
+                        <div class="col-sm-8 col-sm-offset-4">
+                           <img src="/assets/img/logo/logo-white-with-icon.png" class="img-responsive logo" width=""></div>
                     </div>
                     <div class="row">
                         <div class="col-sm-6 col-sm-offset-3 form-box">
@@ -39,7 +32,7 @@
 			                        	<label class="sr-only" for="form-password">Password</label>
 			                        	<input type="password" name="form-password" placeholder="Password..." class="form-password form-control" id="form-password">
 			                        </div>
-			                        <button type="submit" class="btn">Sign in!</button>
+			                        <button type="submit" class="btn raised">Sign in!</button>
 			                    </form>
 		                    </div>
                         </div>
