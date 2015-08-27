9ae8b58ec88f77c8803473d78817469c07604c65
Madi <martha@solutionsresource.com>
2015-08-27 15:24:25 +0800

    [TASK] Made contact number nullable

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->integer('role')->nullable();
-            $table->String('contact_number', 45);
+            $table->String('contact_number', 45)->nullable();
             $table->rememberToken();
          // $table->timestamps();
         });
