0641877f1472d27ddc3bde613898cf10a65757d6
Yannamae <yannationgson@gmail.com>
2015-09-28 13:47:01 +0800

    [TASK] department display

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -17,7 +17,7 @@ class DepartmentController extends Controller
      public function __construct()
     {
         $this->middleware('auth');
-        $this->middleware('admin');
+        $this->middleware('admin',['except'=>'index']);
     }
     /**
      * Display a listing of the resource.
